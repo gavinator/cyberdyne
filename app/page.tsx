a936ba376c9ea79ef36369b7adc710587043b5a3
gavinator <gavin.m.platt@gmail.com>
2026-06-26 09:23:51 -0700

    quoter spacing

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            The future's not set. There's no fate but what we make for ourselves. –John Connor
+            The future's not set. There's no fate but what we make for ourselves.<br></br><br></br>–John Connor
           </h1>
         </div>
       </main>
